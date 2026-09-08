7c6fa3df2a22b134cf1038a0791e7f016e35bfef
MalekZishan <malekzishanrndev1110@gmail.com>
2026-09-08 15:47:57 +0530

    refactor: simplify token verification, optimize image configuration, and add Vercel deployment metadata

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/"
     },
-    sitemap: "/sitemap.xml"
+    sitemap: "https://subhanacademy.in/sitemap.xml"
   };
 }
